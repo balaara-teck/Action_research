5629bcc78684b4d8f4dd71f2a2be95fe52a1986b
balaara-teck <balaarasolomon9@gmail.com>
2025-06-22 22:07:37 +0000

    remove decouple

--- engine/settings.py
+++ engine/settings.py
@@ -1,6 +1,6 @@
 
 from pathlib import Path
-from decouple import config
+
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,13 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
+
 SECRET_KEY = "django-insecure-5brf9=7-xcl7no1$1i0lmzpiz#^w8^5m46!ar!%byanr&&o_(g"
 DEBUG=True
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
